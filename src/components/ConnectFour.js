c4c68753c94cc2e8ef47aa31bad3640ae9686a14
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-07-07 20:00:32 -0300

    added win logic

--- src/components/ConnectFour.js
+++ src/components/ConnectFour.js
@@ -1,22 +1,104 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Grid from './Grid';
 
 const RED = 'red';
 const BLUE = 'blue';
-const GRID = [[RED, RED, BLUE], [], [], [], [], [], []];
+const GRID = [[], [], [], [], [], [], []];
+
+const checkVertical = (
+  currentPlayer,
+  column,
+  disksAmount
+) => {
+  disksAmount = disksAmount || 0;
+  const index = column.length - disksAmount - 1;
+  if (column[index] === currentPlayer) {
+    if (
+      disksAmount === 3 ||
+      checkVertical(currentPlayer, column, disksAmount + 1)
+    ) {
+      column[index] = currentPlayer + '-win';
+      return true;
+    }
+  }
+  return false;
+};
+
+const checkHorizontal = (
+  currentPlayer,
+  grid,
+  column,
+  row,
+  disksAmount
+) => {
+  disksAmount = disksAmount || 0;
+  if (grid[column] && grid[column][row] === currentPlayer) {
+    if (
+      checkHorizontal(
+        currentPlayer,
+        grid,
+        column + 1,
+        row,
+        disksAmount + 1
+      ) ||
+      disksAmount >= 3
+    ) {
+      grid[column][row] = currentPlayer + '-win';
+      return true;
+    }
+  }
+  return false;
+};
+
+const goLeft = (currentPlayer, grid, column, row) => {
+  if (grid[column] && grid[column][row] === currentPlayer) {
+    if (!goLeft(currentPlayer, grid, column - 1, row)) {
+      checkHorizontal(currentPlayer, grid, column, row);
+    }
+  }
+  return false;
+};
+
+const checkWin = (currentPlayer, grid, columnIndex) => {
+  if (checkVertical(currentPlayer, grid[columnIndex])) {
+    return true;
+  }
+  goLeft(
+    currentPlayer,
+    grid,
+    columnIndex,
+    grid[columnIndex].length - 1
+  );
+};
 
 const App = () => {
   const [grid, setGrid] = useState(GRID);
-  const [player, setPlayer] = useState(RED);
-  const handleDrop = index => {
+  const [currentPlayer, setCurrentPlayer] = useState(RED);
+  const handleDrop = (index) => {
     if (grid[index].length > 5) {
       return undefined;
     }
-    grid[index].push(player);
-    setPlayer(player === RED ? BLUE : RED);
-    setGrid([...grid]);
+    const updatedGrid = [...grid];
+    updatedGrid[index] = [
+      ...updatedGrid[index],
+      currentPlayer,
+    ];
+    console.log(
+      checkWin(currentPlayer, updatedGrid, index)
+    );
+    setCurrentPlayer(currentPlayer === RED ? BLUE : RED);
+    setGrid(updatedGrid);
   };
-  return <Grid grid={grid} setGrid={setGrid} onDrop={handleDrop} />;
+  return (
+    <div>
+      <h3>Is player {currentPlayer} turn!</h3>
+      <Grid
+        grid={grid}
+        setGrid={setGrid}
+        onDrop={handleDrop}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,46 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const COLOR = { red: 'hsl(348, 100%, 61%)', blue: 'hsl(217, 71%, 53%)' };
+const COLOR = {
+  red: 'hsl(348, 100%, 61%)',
+  blue: 'hsl(217, 71%, 53%)',
+};
 const LIGHT = 'hsl(0, 0%, 96%)';
 const DARK = 'hsl(0, 0%, 86%)';
 const SIZE = 2;
 
 const Disk = styled.div`
-	width: ${SIZE}em;
-	height: ${SIZE}em;
-	border-radius: ${SIZE}em;
-	background-color: ${({ color }) => COLOR[color]};
-	margin-top: 1px;
+  width: ${SIZE}em;
+  height: ${SIZE}em;
+  border-radius: ${SIZE}em;
+  border: 2px solid
+    ${({ color }) =>
+      !color.includes('-win') ? COLOR[color] : 'black'};
+  background-color: ${({ color }) =>
+    COLOR[color.replace('-win', '')]};
+  margin-top: 1px;
 `;
 
 const Column = styled.div`
-	width: ${SIZE}em;
-	height: ${SIZE * 6.5}em;
-	padding: 2px;
-	background-color: ${({ index }) => (index % 2 === 0 ? LIGHT : DARK)};
+display: flex;
+flex-direction: column-reverse;
+  width: ${SIZE}em;
+  height: ${SIZE * 7}em;
+  padding: 2px 6px 2px 2px
+  background-color: ${({ index }) =>
+    index % 2 === 0 ? LIGHT : DARK};
 `;
 
 const Container = styled.div`
-	display: flex;
-	transform: rotate(180deg);
-	width: fit-content;
-	border: 2px solid black;
-	border-radius: 5px;
-	overflow: hidden;
-	box-sizing: unset;
+  left: 0;
+  right: 0;
+  margin: auto;
+  display: flex;
+  width: fit-content;
+  border: 2px solid black;
+  border-radius: 5px;
+  overflow: hidden;
+  box-sizing: unset;
 `;
 
 const Grid = ({ grid, onDrop }) => (
-	<Container>
-		{grid.map((column, index) => (
-			<Column key={index} index={index} onClick={() => onDrop(index)}>
-				{column.map((color, index) => (
-					<Disk key={index} color={color} />
-				))}
-			</Column>
-		))}
-	</Container>
+  <Container>
+    {grid.map((column, index) => (
+      <Column
+        key={index}
+        index={index}
+        onClick={() => onDrop(index)}
+      >
+        {column.map((color, index) => (
+          <Disk key={index} color={color} />
+        ))}
+      </Column>
+    ))}
+  </Container>
 );
 
 export default Grid;
