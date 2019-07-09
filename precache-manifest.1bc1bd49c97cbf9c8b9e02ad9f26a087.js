2ca2250c163125d317167a22e3843961e30eefb4
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-07-09 14:14:51 -0300

    Updates

---
+++ precache-manifest.1bc1bd49c97cbf9c8b9e02ad9f26a087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba760b8a01284020a19a22811b062ff",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "11c04cdec128f713e88b",
+    "url": "/connect-four/static/css/2.f278687d.chunk.css"
+  },
+  {
+    "revision": "11c04cdec128f713e88b",
+    "url": "/connect-four/static/js/2.cef4e133.chunk.js"
+  },
+  {
+    "revision": "5261fa93a625fa3b6ebc",
+    "url": "/connect-four/static/js/main.f9d0fd94.chunk.js"
+  },
+  {
+    "revision": "5f78906a733342f2d383",
+    "url": "/connect-four/static/js/runtime~main.000e00dd.js"
+  }
+]);
